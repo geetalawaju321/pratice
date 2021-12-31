a5c253d51735d0be5f196275335eda18eb6fd009
geetalawaju321 <geetalawaju321@gmail.com>
2021-12-31 16:09:54 -0600

    square or recatangle is created

---
+++ src/com/from/zenhub/SquareOrRectangle.java
@@ -0,0 +1,31 @@
+package com.from.zenhub;
+
+import java.util.Scanner;
+
+public class SquareOrRectangle {
+
+	public static void main(String[] args) {
+		int length;
+		int breadth;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the length: ");
+		length = scanner.nextInt();
+		
+		System.out.println("Enter the breath:");
+		breadth = scanner.nextInt();
+		
+		if(length==breadth){
+			System.out.println("The geometry shape is Square.");
+			
+		}
+		else {
+			System.out.println("The geometry shape is Rectangle.");
+		}
+		int a = length*breadth;
+		int p = 2*(length + breadth);
+		
+		System.out.println("The area of geometry shape is:" + a + " and the perimeter of the square is: " + p);	
+	}
+
+}
