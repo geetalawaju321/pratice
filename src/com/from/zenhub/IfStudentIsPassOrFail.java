f3ee1cba3df509e5c026d91855fbe8fc183bf758
geetalawaju321 <geetalawaju321@gmail.com>
2022-01-04 10:59:55 -0600

    22 checked

---
+++ src/com/from/zenhub/IfStudentIsPassOrFail.java
@@ -0,0 +1,19 @@
+package com.from.zenhub;
+
+import java.util.Scanner;
+
+public class IfStudentIsPassOrFail {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Provide Marks: ");
+		int x = scanner.nextInt();
+		
+		if (x >= 40){
+			System.out.println("Congratulations, You are Pass.");
+		}else if (x <= 40){
+			System.out.println("You are Fail.Better luck next time.");
+		}
+		scanner.close();
+	}
+}
