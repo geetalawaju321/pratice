0777ce7764042b91017e384a754453bfaecce688
geetalawaju321 <geetalawaju321@gmail.com>
2022-01-04 11:17:08 -0600

    created 23

---
+++ src/com/from/zenhub/IfNumberIsPositiveOrNegative.java
@@ -0,0 +1,21 @@
+package com.from.zenhub;
+
+import java.util.Scanner;
+
+public class IfNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Give any integer: ");
+		int x = scanner.nextInt();
+		
+		if ( x > 0){
+			System.out.println(x + " is a postive number.");
+		}else if ( x < 0){
+			System.out.println(x + " is a negative number.");	
+		}else if ( x == 0){
+			System.out.println(x + " is neither positive nor negative.");
+		}
+		scanner.close();
+	}
+}
