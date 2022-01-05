09e32147a11322526ec21083aa06e94bd73bb032
geetalawaju321 <geetalawaju321@gmail.com>
2022-01-05 15:12:01 -0600

    create #35

---
+++ src/com/from/zenhub/GeetaMath.java
@@ -0,0 +1,9 @@
+package com.from.zenhub;
+
+public interface GeetaMath {
+	int add (int n1,int n2);
+	int subtract(int n1,int n2);
+	int max(int n1,int n2);
+	int min(int n1,int n2);
+	int abs(int n1);
+}
