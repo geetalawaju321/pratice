296d5fa3c66490319f48771ad7554fcac50f4d11
geetalawaju321 <geetalawaju321@gmail.com>
2022-01-17 18:43:52 -0600

    created#40

--- src/com/from/zenhub/RemoveDuplicate.java
+++ src/com/from/zenhub/RemoveDuplicate.java
@@ -9,26 +9,11 @@ public class RemoveDuplicate {
 		Scanner scanner = new Scanner(System.in);
 		HashSet<Integer> hashSet = new HashSet<Integer>();
 		
-		System.out.println("Enter the number 1 : ");
-		int intOne = scanner.nextInt();
-		hashSet.add(intOne);
-		
-		System.out.println("Enter the number 2 : ");
-		int intTwo = scanner.nextInt();
-		hashSet.add(intTwo);
-		
-		System.out.println("Enter the number 3 : ");
-		int intThree = scanner.nextInt();
-		hashSet.add(intThree);
-		
-		System.out.println("Enter the number 4 : ");
-		int intFour = scanner.nextInt();
-		hashSet.add(intFour);
-		
-		System.out.println("Enter the number 5 : ");
-		int intFive = scanner.nextInt();
-		hashSet.add(intFive);
-		
+		for (int x = 1;x <= 5; x++){
+			System.out.println("Enter the number " + x +" : ");
+			int value = scanner.nextInt();
+			hashSet.add(value);
+		}
 		System.out.println(hashSet);
 	}
 }
